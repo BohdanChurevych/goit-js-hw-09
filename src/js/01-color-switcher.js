a2771108d413c6e8c5db24e2edc6a087a37b54df
BohdanChurevych <bohdan.churevych@gmail.com>
2023-03-05 20:36:15 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const openBtn = document.querySelector('button[data-start]');
+const closeBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervalid = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor(evt) {
+    openBtn.setAttribute('disabled', true);
+    intervalid = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+openBtn.addEventListener('click', changeColor);
+closeBtn.addEventListener('click', () => {
+  clearInterval(intervalid);
+  openBtn.removeAttribute('disabled');
+});
